0adb28209ea3b3444c4cde19a9786b1ab930ceea
rendyproklamanta <rendydwi@myself.com>
2022-06-17 11:44:06 +0700

    update

--- stunnel5/ssh-ssl.sh
+++ stunnel5/ssh-ssl.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 # ==================================================
 # Link Hosting Kalian
-akbarvpn=""raw.githubusercontent.com/rendyproklamanta/vps-admin/main/ssh
+akbarvpn="raw.githubusercontent.com/rendyproklamanta/vps-admin/main/ssh"
 
 # Link Hosting Kalian Untuk Xray
 akbarvpnn="raw.githubusercontent.com/rendyproklamanta/vps-admin/main/xray"
